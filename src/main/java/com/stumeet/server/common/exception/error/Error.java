4e32cdc9ddc61caceeabdf74707662327ab08bb5
Lee ChanMi <anytime0224@gmail.com>
2024-02-20 15:05:53 +0900

    [STMT-6] :adhesive_bandage: Error 클래스 json index 삭제

--- src/main/java/com/stumeet/server/common/exception/error/Error.java
+++ src/main/java/com/stumeet/server/common/exception/error/Error.java
@@ -1,10 +1,7 @@
 package com.stumeet.server.common.exception.error;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Error {
 
-    @JsonProperty(index = 2)
     private final String message;
 
     public Error(String message) {
--- src/main/java/com/stumeet/server/common/exception/error/ErrorField.java
+++ src/main/java/com/stumeet/server/common/exception/error/ErrorField.java
@@ -1,12 +1,10 @@
 package com.stumeet.server.common.exception.error;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.validation.BindingResult;
 public class ErrorField extends Error {
 
-    @JsonProperty(index = 1)
     final String code;
 
     public ErrorField(String message, String code) {
