259c562b060ee142608c3c28ba29f44a65861a3e
yongckim <zxcv9203@naver.com>
2024-02-26 23:06:27 +0900

    :white_check_mark: [STMT-146] 테스트 코드를 통한 문서화를 위한 Restdocs 작성

--- src/test/java/com/stumeet/server/common/auth/filter/OAuthAuthenticationFilterTest.java
+++ src/test/java/com/stumeet/server/common/auth/filter/OAuthAuthenticationFilterTest.java
@@ -3,6 +3,7 @@ package com.stumeet.server.common.auth.filter;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.stumeet.server.common.auth.model.AuthenticationHeader;
+import com.stumeet.server.member.domain.OAuthProvider;
 import com.stumeet.server.stub.MemberStub;
 import com.stumeet.server.template.ApiTest;
 import com.stumeet.server.template.RedisTestConfig;
@@ -11,12 +12,18 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.cloud.contract.spec.internal.MediaTypes.APPLICATION_JSON;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WireMockTest(httpPort = 8089)
@@ -29,29 +36,43 @@ class OAuthAuthenticationFilterTest extends ApiTest {
     class oauthLogin {
         private final String path = "/api/v1/oauth";
 
-        private static void mockKakaoTokenInfoApi() {
+        @Test
+        @DisplayName("[성공] 소셜 로그인(카카오) 성공")
+        void successKakaoTest() throws Exception {
+            mockKakaoTokenInfoApi();
+
+            mockMvc.perform(post(path)
+                            .header(AuthenticationHeader.ACCESS_TOKEN.getName(), MemberStub.getKakaoAccessToken())
+                            .header(AuthenticationHeader.X_OAUTH_PROVIDER.getName(), OAuthProvider.KAKAO.getProvider()))
+                    .andExpect(status().isOk())
+                    .andDo(document("social_login/success",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("OAuth Provider로 부터 전달받은 토큰"),
+                                            headerWithName(AuthenticationHeader.X_OAUTH_PROVIDER.getName()).description("OAuth Provider 이름")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답에 대한 결과 코드"),
+                                            fieldWithPath("message").type(JsonFieldType.STRING).description("응답에 대한 메시지"),
+                                            fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("액세스 토큰"),
+                                            fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"),
+                                            fieldWithPath("data.isFirstLogin").type(JsonFieldType.BOOLEAN).description("최초 로그인 여부")
+                                    )
+                            )
+                    );
+        }
+
+        private void mockKakaoTokenInfoApi() {
             stubFor(
                     WireMock.get(WireMock.urlEqualTo("/v1/user/access_token_info"))
-                            .withHeader(AuthenticationHeader.ACCESS_TOKEN.getName(), equalTo("rjdizj7Ae09H0oWlW46Oll9_x7AhzaJkp1gKKwzTAAABjd_1h0EhI_W2iN1234"))
+                            .withHeader(AuthenticationHeader.ACCESS_TOKEN.getName(), equalTo(MemberStub.getKakaoAccessToken()))
                             .willReturn(aResponse()
                                     .withStatus(HttpStatus.CREATED.value())
                                     .withHeader("content-type", APPLICATION_JSON)
                                     .withBody(MemberStub.getKakaoAccessTokenInfo())
                             )
             );
-        }
-
-        @Test
-        @DisplayName("[성공] 소셜 로그인 성공")
-        void successTest() throws Exception {
-            mockKakaoTokenInfoApi();
-
-            MvcResult result = mockMvc.perform(
-                            post(path)
-                                    .header(AuthenticationHeader.ACCESS_TOKEN.getName(), "rjdizj7Ae09H0oWlW46Oll9_x7AhzaJkp1gKKwzTAAABjd_1h0EhI_W2iN1234")
-                                    .header(AuthenticationHeader.X_OAUTH_PROVIDER.getName(), "kakao")
-                    ).andExpect(status().isOk())
-                    .andReturn();
         }
     }
 }
--- src/test/java/com/stumeet/server/stub/MemberStub.java
+++ src/test/java/com/stumeet/server/stub/MemberStub.java
@@ -9,4 +9,8 @@ public class MemberStub {
     public static String getKakaoAccessTokenInfo() {
         return "{\"expiresInMillis\":20978166,\"id\":1,\"expires_in\":20978,\"app_id\":1,\"appId\":1}";
     }
+
+    public static String getKakaoAccessToken() {
+        return "rjdizj7Ae09H0oWlW46Oll9_x7AhzaJkp1gKKwzTAAABjd_1h0EhI_W2iN1234";
+    }
 }
