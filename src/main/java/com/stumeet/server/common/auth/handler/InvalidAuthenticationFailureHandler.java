a4aade8c957bdbed7e873bdbc5a6d49de1bed7b9
zxcv9203 <41960243+zxcv9203@users.noreply.github.com>
2024-02-27 21:09:05 +0900

    :white_check_mark: [STMT-146] 소셜 로그인시 인증 실패에 대한 상황에 대한 테스트 추가 (#46)

--- src/main/java/com/stumeet/server/common/auth/handler/InvalidAuthenticationFailureHandler.java
+++ src/main/java/com/stumeet/server/common/auth/handler/InvalidAuthenticationFailureHandler.java
@@ -23,13 +23,14 @@ public class InvalidAuthenticationFailureHandler implements AuthenticationFailur
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         log.warn(exception.getMessage());
+        log.debug("", exception);
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
 
         ApiResponse<Void> apiResponse = ApiResponse.fail(
                 HttpStatus.UNAUTHORIZED.value(),
-                "OAuth 인증에 실패했습니다."
+                exception.getMessage()
         );
 
         response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
