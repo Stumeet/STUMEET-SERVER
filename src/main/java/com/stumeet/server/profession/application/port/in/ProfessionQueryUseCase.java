08bc8c7b6bc67730afd74ad0d50a1c0987bb6a4e
yongckim <zxcv9203@naver.com>
2024-02-28 23:11:37 +0900

    :sparkles: [STMT-146] 분야 정보를 전부 조회하는 API 구현

--- src/main/java/com/stumeet/server/profession/application/port/in/ProfessionQueryUseCase.java
+++ src/main/java/com/stumeet/server/profession/application/port/in/ProfessionQueryUseCase.java
@@ -1,7 +1,10 @@
 package com.stumeet.server.profession.application.port.in;
 
+import com.stumeet.server.profession.adapter.in.web.response.ProfessionResponses;
 import com.stumeet.server.profession.domain.Profession;
 
 public interface ProfessionQueryUseCase {
     Profession getById(Long id);
+
+    ProfessionResponses findAll();
 }
