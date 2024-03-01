8d2e99f5ddaa1e76f25e0c09befd167c03121b1e
yongckim <zxcv9203@naver.com>
2024-03-01 14:52:17 +0900

    :recycle: [STMT-146] 테스트 코드 실행시에는 해당 설정이 등록되지 않도록 처리

--- src/main/java/com/stumeet/server/common/config/AwsS3Config.java
+++ src/main/java/com/stumeet/server/common/config/AwsS3Config.java
@@ -3,40 +3,39 @@ package com.stumeet.server.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@RequiredArgsConstructor
+@Profile("!test")
 public class AwsS3Config {
 
-	@Value("${spring.cloud.config.server.awss3.region}")
-	private String region;
+    @Value("${spring.cloud.config.server.awss3.region}")
+    private String region;
 
-	@Value("${spring.cloud.config.server.awss3.credentials.access-key}")
-	private String accessKey;
+    @Value("${spring.cloud.config.server.awss3.credentials.access-key}")
+    private String accessKey;
 
-	@Value("${spring.cloud.config.server.awss3.credentials.secret-key}")
-	private String secretKey;
+    @Value("${spring.cloud.config.server.awss3.credentials.secret-key}")
+    private String secretKey;
 
-	private AwsBasicCredentials awsBasicCredentials() {
-		return AwsBasicCredentials.create(accessKey, secretKey);
-	}
+    private AwsBasicCredentials awsBasicCredentials() {
+        return AwsBasicCredentials.create(accessKey, secretKey);
+    }
 
-	@Bean
-	public StaticCredentialsProvider staticCredentialsProvider() {
-		return StaticCredentialsProvider.create(awsBasicCredentials());
-	}
+    @Bean
+    public StaticCredentialsProvider staticCredentialsProvider() {
+        return StaticCredentialsProvider.create(awsBasicCredentials());
+    }
 
-	@Bean
-	public S3Client s3Client() {
-		return S3Client.builder()
-			.credentialsProvider(staticCredentialsProvider())
-			.region(Region.of(region))
-			.build();
-	}
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .credentialsProvider(staticCredentialsProvider())
+                .region(Region.of(region))
+                .build();
+    }
 }
