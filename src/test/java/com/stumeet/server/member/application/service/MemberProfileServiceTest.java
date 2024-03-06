e639c92458dd4d2a0de1caf72e70d1e0ecf889f6
yongckim <zxcv9203@naver.com>
2024-03-06 20:55:48 +0900

    :white_check_mark: [STMT-199] 멤버 정보 조회 유즈 케이스 테스트 코드 작성

--- src/test/java/com/stumeet/server/member/application/service/MemberProfileServiceTest.java
+++ src/test/java/com/stumeet/server/member/application/service/MemberProfileServiceTest.java
@@ -1,8 +1,11 @@
 package com.stumeet.server.member.application.service;
 
 import com.stumeet.server.file.application.port.in.FileUploadUseCase;
+import com.stumeet.server.member.adapter.in.web.response.MemberProfileResponse;
 import com.stumeet.server.member.application.port.in.command.MemberUpdateCommand;
+import com.stumeet.server.member.application.port.in.mapper.MemberUseCaseMapper;
 import com.stumeet.server.member.application.port.out.MemberCommandPort;
+import com.stumeet.server.member.application.port.out.MemberQueryPort;
 import com.stumeet.server.member.domain.Member;
 import com.stumeet.server.profession.application.port.in.ProfessionQueryUseCase;
 import com.stumeet.server.stub.FileStub;
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -32,6 +36,12 @@ class MemberProfileServiceTest extends UnitTest {
 
     @Mock
     private ProfessionQueryUseCase professionQueryUseCase;
+
+    @Mock
+    private MemberQueryPort memberQueryPort;
+
+    @Mock
+    private MemberUseCaseMapper memberUseCaseMapper;
 
     @Nested
     @DisplayName("멤버 프로필 업데이트")
@@ -51,6 +61,26 @@ class MemberProfileServiceTest extends UnitTest {
             memberProfileService.updateProfile(member, command);
 
             then(memberCommandPort).should().update(member);
+        }
+    }
+
+    @Nested
+    @DisplayName("프로필 조회")
+    class GetProfileById {
+        @Test
+        @DisplayName("[성공] 멤버 프로필 조회에 성공한다.")
+        void successTest() {
+            Member member = MemberStub.getMember();
+            MemberProfileResponse want = MemberStub.getMemberProfileResponse(member);
+
+            given(memberQueryPort.getById(member.getId()))
+                    .willReturn(member);
+            given(memberUseCaseMapper.toProfileResponse(member))
+                    .willReturn(want);
+
+            MemberProfileResponse got = memberProfileService.getProfileById(member.getId());
+
+            assertThat(got).usingRecursiveComparison().isEqualTo(want);
         }
     }
 
