11f93a1961342d66b543df49b517b206f972024b
yongckim <zxcv9203@naver.com>
2024-03-06 20:52:43 +0900

    :sparkles: [STMT-199] 내 정보 조회 API 구현

--- src/main/java/com/stumeet/server/member/adapter/in/web/MemberProfileApi.java
+++ src/main/java/com/stumeet/server/member/adapter/in/web/MemberProfileApi.java
@@ -3,6 +3,7 @@ package com.stumeet.server.member.adapter.in.web;
 import com.stumeet.server.common.annotation.WebAdapter;
 import com.stumeet.server.common.auth.model.LoginMember;
 import com.stumeet.server.common.model.ApiResponse;
+import com.stumeet.server.member.adapter.in.web.response.MemberProfileResponse;
 import com.stumeet.server.member.application.port.in.MemberProfileUseCase;
 import com.stumeet.server.member.application.port.in.command.MemberUpdateCommand;
 import jakarta.validation.Valid;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,6 +30,17 @@ public class MemberProfileApi {
         memberProfileUseCase.updateProfile(member.getMember(), request);
         return new ResponseEntity<>(
                 ApiResponse.success(HttpStatus.OK.value(), "내 프로필 수정에 성공했습니다."),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<MemberProfileResponse>> getMyProfile(
+            @AuthenticationPrincipal LoginMember member
+    ) {
+        MemberProfileResponse response = memberProfileUseCase.getProfileById(member.getMember().getId());
+        return new ResponseEntity<>(
+                ApiResponse.success(HttpStatus.OK.value(), "내 프로필 조회에 성공했습니다.", response),
                 HttpStatus.OK
         );
     }
