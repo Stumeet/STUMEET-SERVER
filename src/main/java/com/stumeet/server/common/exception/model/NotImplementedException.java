1bba4cfa0a1f339b8ce9b74660da3fbe7347d7ec
이찬미 <anytime0224@gmail.com>
2024-03-07 15:30:10 +0900

    🚧 [STMT-146] 리뷰 반영: 띄어쓰기
    Co-authored-by: zxcv9203 <41960243+zxcv9203@users.noreply.github.com>

--- src/main/java/com/stumeet/server/common/exception/model/NotImplementedException.java
+++ src/main/java/com/stumeet/server/common/exception/model/NotImplementedException.java
@@ -2,7 +2,7 @@ package com.stumeet.server.common.exception.model;
 
 import com.stumeet.server.common.response.ErrorCode;
 
-public class NotImplementedException extends BusinessException{
+public class NotImplementedException extends BusinessException {
 	public NotImplementedException() {
 		super(ErrorCode.NOT_IMPLEMENTED_ERROR);
 	}
