bf5b2e007fc5f373435a3d61149e1a3dafb688b2
Lee ChanMi <anytime0224@gmail.com>
2024-03-08 00:19:57 +0900

    :sparkles: [STMT-200] Study id로 가져오기 API 구현

--- src/main/java/com/stumeet/server/common/response/SuccessCode.java
+++ src/main/java/com/stumeet/server/common/response/SuccessCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessCode {
 
+    GET_SUCCESS(HttpStatus.OK, "조회에 성공했습니다."),
     SIGN_UP_SUCCESS(HttpStatus.CREATED, "회원가입에 성공했습니다.")
     ;
 
---
+++ src/main/java/com/stumeet/server/study/adapter/in/web/StudyQueryApi.java
@@ -0,0 +1,35 @@
+package com.stumeet.server.study.adapter.in.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stumeet.server.common.annotation.WebAdapter;
+import com.stumeet.server.common.model.ApiResponse;
+import com.stumeet.server.common.response.SuccessCode;
+import com.stumeet.server.study.adapter.in.web.response.StudyDetailResponse;
+import com.stumeet.server.study.application.port.in.StudyQueryUseCase;
+import com.stumeet.server.study.application.port.out.mapper.StudyUseCaseMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@WebAdapter
+@RequestMapping("/api/v1/studies")
+@RequiredArgsConstructor
+public class StudyQueryApi {
+
+	private final StudyQueryUseCase studyQueryUseCase;
+
+	private final StudyUseCaseMapper studyUseCaseMapper;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiResponse<StudyDetailResponse>> getStudy(
+		@PathVariable(name = "id") Long id
+	) {
+		StudyDetailResponse response =
+			studyUseCaseMapper.toStudyDetailResponse(studyQueryUseCase.getById(id));
+
+		return ResponseEntity.ok(ApiResponse.success(SuccessCode.GET_SUCCESS, response));
+	}
+}
