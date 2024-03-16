06af68c4451152628c35e8c67e28b596c57332cf
Lee ChanMi <anytime0224@gmail.com>
2024-03-16 22:03:19 +0900

    :card_file_box: [STMT-200] 스터디 관련 DDL 수정

--- src/main/resources/db/migration/V1.4__add_study_table.sql
+++ src/main/resources/db/migration/V1.4__add_study_table.sql
@@ -6,28 +6,53 @@ CREATE TABLE `study_field`
       DEFAULT CHARSET = utf8mb4
       COLLATE = utf8mb4_unicode_ci;
 
-CREATE TABLE `study`
+CREATE TABLE `study_domain`
 (
-    `id`             BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 ID',
-    `study_field_id` BIGINT NULL COMMENT '스터디 분야 ID',
-    `name`           VARCHAR(255) NOT NULL COMMENT '스터디명',
-    `tags`           VARCHAR(100) NULL COMMENT '태그',
-    `region`         VARCHAR(50)  NOT NULL COMMENT '활동 지역',
-    `intro`          VARCHAR(100) NOT NULL COMMENT '소개',
-    `rule`           VARCHAR(100) NULL COMMENT '규칙',
-    `image`          VARCHAR(500) NOT NULL COMMENT '스터디 이미지의 URL',
-    `headcount`      TINYINT(50) NOT NULL DEFAULT 1 COMMENT '인원 수',
-    `start_date`     DATETIME     NOT NULL COMMENT '시작일',
-    `end_date`       DATETIME     NOT NULL COMMENT '종료일',
-    `created_at`     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
-    `updated_at`     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
-    `is_finished`    TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 완료 여부',
-    `is_deleted`     TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 삭제 여부',
-    `deleted_at`     DATETIME NULL COMMENT '스터디 삭제 일자',
+    `id`             BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 태그 ID',
+    `study_field_id` BIGINT NOT NULL COMMENT '스터디 분야 ID',
 
     CONSTRAINT fk_study_field FOREIGN KEY (study_field_id)
         REFERENCES study_field (id)
         ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE = InnoDB
+      DEFAULT CHARSET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `study`
+(
+    `id`              BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 ID',
+    `study_domain_id` BIGINT       NOT NULL COMMENT '스터디 도메인 ID',
+    `name`            VARCHAR(255) NOT NULL COMMENT '스터디명',
+    `region`          VARCHAR(50)  NOT NULL COMMENT '활동 지역',
+    `intro`           VARCHAR(100) NOT NULL COMMENT '소개',
+    `rule`            VARCHAR(100) NULL COMMENT '규칙',
+    `image`           VARCHAR(500) NOT NULL COMMENT '스터디 이미지의 URL',
+    `headcount`       TINYINT(50) NOT NULL DEFAULT 1 COMMENT '인원 수',
+    `start_date`      DATETIME     NOT NULL COMMENT '시작일',
+    `end_date`        DATETIME     NOT NULL COMMENT '종료일',
+    `created_at`      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    `updated_at`      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+    `is_finished`     TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 완료 여부',
+    `is_deleted`      TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 삭제 여부',
+    `deleted_at`      DATETIME NULL COMMENT '스터디 삭제 일자',
+
+    CONSTRAINT fk_study_domain_with_study FOREIGN KEY (study_domain_id)
+        REFERENCES study_domain (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB
+      DEFAULT CHARSET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+
+
+CREATE TABLE `study_tag`
+(
+    `id`              BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 태그 ID',
+    `study_domain_id` BIGINT      NOT NULL COMMENT '스터디 관련 ID',
+    `name`            VARCHAR(10) NOT NULL COMMENT '태그명',
+
+    CONSTRAINT fk_study_domain_with_tag FOREIGN KEY (study_domain_id)
+        REFERENCES study_domain (id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
       DEFAULT CHARSET = utf8mb4
       COLLATE = utf8mb4_unicode_ci;
--- src/main/resources/db/migration/V1.5_insert_dummy_study_data.sql
+++
@@ -1,7 +0,0 @@
-insert into study_field value (1, 'IT');
-
-INSERT INTO study (study_field_id, name, region, tags, intro, rule, image, start_date, end_date)
-VALUES (1, '[임시] 프로그래밍 스터디', '서울', 'java;springboot;프로그래밍;',
-        '프로그래밍 스터디 입니다.', '- 매주 목요일 8시\n- 장소: 안암역\n- 제시간에 제출하기!',
-        'https://stumeet.s3.ap-northeast-2.amazonaws.com/study/1/image/2023062711172178420.png',
-        '2024-04-01', '2024-05-01');
