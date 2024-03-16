b076ea530ca945b52be0518096d60d117813958d
Lee ChanMi <anytime0224@gmail.com>
2024-03-16 22:00:10 +0900

    :sparkles: [STMT-200] 각 entity의 mapper 구현

--- src/main/java/com/stumeet/server/study/adapter/out/persistance/mapper/StudyPersistenceMapper.java
+++ src/main/java/com/stumeet/server/study/adapter/out/persistance/mapper/StudyPersistenceMapper.java
@@ -6,7 +6,6 @@ import com.stumeet.server.study.adapter.out.persistance.entity.StudyJpaEntity;
 import com.stumeet.server.study.domain.Study;
 import com.stumeet.server.study.domain.StudyHeadCount;
 import com.stumeet.server.study.domain.StudyPeriod;
-import com.stumeet.server.study.domain.StudyTags;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,40 +13,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class StudyPersistenceMapper {
 
-	private final StudyFieldPersistenceMapper studyFieldPersistenceMapper;
-
-	public StudyJpaEntity toEntity(Study domain) {
-		return StudyJpaEntity.builder()
-			.id(domain.getId())
-			.name(domain.getName())
-			.studyField(studyFieldPersistenceMapper.toEntity(domain.getStudyField()))
-			.tags(domain.getAssembledTags())
-			.region(domain.getRegion())
-			.intro(domain.getIntro())
-			.rule(domain.getRule())
-			.startDate(domain.getStartDate())
-			.endDate(domain.getEndDate())
-			.headcount(domain.getHeadcountNumber())
-			.image(domain.getImage())
-			.isFinished(domain.getIsFinished())
-			.isDeleted(domain.getIsDeleted())
-			.build();
-	}
+	private final StudyDomainPersistenceMapper studyDomainPersistenceMapper;
 
 	public Study toDomain(StudyJpaEntity entity) {
 		return Study.builder()
-			.id(entity.getId())
-			.name(entity.getName())
-			.studyField(studyFieldPersistenceMapper.toDomain(entity.getStudyField()))
-			.studyTags(StudyTags.from(entity.getTags()))
-			.region(entity.getRegion())
-			.intro(entity.getIntro())
-			.rule(entity.getRule())
-			.period(StudyPeriod.of(entity.getStartDate(), entity.getEndDate()))
-			.headcount(StudyHeadCount.from(entity.getHeadcount()))
-			.image(entity.getImage())
-			.isFinished(entity.getIsFinished())
-			.isDeleted(entity.getIsDeleted())
-			.build();
+				.id(entity.getId())
+				.name(entity.getName())
+				.studyDomain(studyDomainPersistenceMapper.toDomain(entity.getStudyDomain()))
+				.region(entity.getRegion())
+				.intro(entity.getIntro())
+				.rule(entity.getRule())
+				.period(StudyPeriod.of(entity.getStartDate(), entity.getEndDate()))
+				.headcount(StudyHeadCount.from(entity.getHeadcount()))
+				.image(entity.getImage())
+				.isFinished(entity.getIsFinished())
+				.isDeleted(entity.getIsDeleted())
+				.build();
+	}
+
+	public StudyJpaEntity toEntity(Study domain) {
+		return StudyJpaEntity.builder()
+				.id(domain.getId())
+				.name(domain.getName())
+				.studyDomain(studyDomainPersistenceMapper.toEntity(domain.getStudyDomain()))
+				.region(domain.getRegion())
+				.intro(domain.getIntro())
+				.rule(domain.getRule())
+				.startDate(domain.getStartDate())
+				.endDate(domain.getEndDate())
+				.headcount(domain.getHeadcountNumber())
+				.image(domain.getImage())
+				.isFinished(domain.isFinished())
+				.isDeleted(domain.isDeleted())
+				.build();
 	}
 }
---
+++ src/main/java/com/stumeet/server/study/adapter/out/persistance/mapper/StudyTagPersistenceMapper.java
@@ -0,0 +1,32 @@
+package com.stumeet.server.study.adapter.out.persistance.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.stumeet.server.study.adapter.out.persistance.entity.StudyTagJpaEntity;
+import com.stumeet.server.study.domain.StudyTag;
+
+@Component
+public class StudyTagPersistenceMapper {
+
+	public List<StudyTag> toDomains(List<StudyTagJpaEntity> entities) {
+		return entities
+				.stream()
+				.map(tagEntity -> StudyTag.builder()
+						.id(tagEntity.getId())
+						.name(tagEntity.getName())
+						.build())
+				.toList();
+	}
+
+	public List<StudyTagJpaEntity> toEntities(List<StudyTag> domains, Long studyDomainId) {
+		return domains.stream()
+				.map(tagDomain -> StudyTagJpaEntity.builder()
+						.id(tagDomain.getId())
+						.studyDomainId(studyDomainId)
+						.name(tagDomain.getName())
+						.build())
+				.toList();
+	}
+}
