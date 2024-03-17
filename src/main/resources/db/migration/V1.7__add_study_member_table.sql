9850beeeb795de389bf06932296c9a609045a6c2
yongckim <zxcv9203@naver.com>
2024-03-17 22:44:47 +0900

    :recycle: [STMT-220] 스터디 멤버 스키마 제약조건 추가 및 생성되어 있는 경우 재생성 하지 않도록 변경

--- src/main/resources/db/migration/V1.7__add_study_member_table.sql
+++ src/main/resources/db/migration/V1.7__add_study_member_table.sql
@@ -1,10 +1,19 @@
-CREATE TABLE `STUDY_MEMBER`
+CREATE TABLE IF NOT EXISTS `study_member`
 (
-    `id`          BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 멤버 ID',
-    `member_id`   BIGINT     NOT NULL COMMENT '멤버 ID',
-    `study_id`    BIGINT     NOT NULL COMMENT '스터디 ID',
-    `is_admin`    TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 관리자 여부',
-    `is_sent_grape`  TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '포도알 전송 여부',
-    `created_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
-    `updated_at`  DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간'
-);
+    `id`            BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '스터디 멤버 ID',
+    `member_id`     BIGINT     NOT NULL COMMENT '멤버 ID',
+    `study_id`      BIGINT     NOT NULL COMMENT '스터디 ID',
+    `is_admin`      TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '스터디 관리자 여부',
+    `is_sent_grape` TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '포도알 전송 여부',
+    `created_at`    DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    `updated_at`    DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+
+    CONSTRAINT `fk_member_id` FOREIGN KEY (`member_id`)
+        REFERENCES `member` (`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_study_id` FOREIGN KEY (`study_id`)
+        REFERENCES `study` (`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
