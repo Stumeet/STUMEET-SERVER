bbd7d631536f1090bde7928a4f4f1037112d5a4a
yongckim <zxcv9203@naver.com>
2024-03-20 22:02:07 +0900

    :recycle: [STMT-220] study_member에 대한 테스트 데이터 추가

--- src/test/resources/db/setup.sql
+++ src/test/resources/db/setup.sql
@@ -1,3 +1,6 @@
 INSERT INTO member (id, name, image, region, profession_id, role, auth_type, tier, experience, is_deleted, deleted_at)
-VALUES (1, 'test', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
+VALUES (1, 'test1', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
         '서울', 1, 'FIRST_LOGIN', 'OAUTH', 'SEED', 0.0, false, null);
+
+INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape)
+VALUES (1, 1, true, false);
