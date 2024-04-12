d5946c351c69f6a51c461d67a2bb4bd34f4e0f1d
05AM <anytime0224@gmail.com>
2024-04-12 23:49:26 +0900

    :fire: [STMT-200] study 스키마 구조 변경으로 인한 불필요한 코드 삭제
    - study field, study domain repository, service 관련 파일 삭제

--- src/main/java/com/stumeet/server/study/adapter/out/persistance/JpaStudyDomainRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stumeet.server.study.adapter.out.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.stumeet.server.study.adapter.out.persistance.entity.StudyDomainJpaEntity;
-
-public interface JpaStudyDomainRepository extends JpaRepository<StudyDomainJpaEntity, Long> {
-}
--- src/main/java/com/stumeet/server/study/application/port/out/StudyDomainCommandPort.java
+++
@@ -1,8 +0,0 @@
-package com.stumeet.server.study.application.port.out;
-
-import com.stumeet.server.study.domain.StudyDomain;
-
-public interface StudyDomainCommandPort {
-
-	StudyDomain save(StudyDomain studyDomain);
-}
