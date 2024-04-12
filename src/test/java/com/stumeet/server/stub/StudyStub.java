87d8556d86c42773194300b9827b1ef9b4aad15f
05AM <anytime0224@gmail.com>
2024-04-13 02:48:45 +0900

    :white_check_mark: [STMT-200] study 관련 테스트 및 stub과 setup, teardown 수정

--- src/test/java/com/stumeet/server/stub/StudyStub.java
+++ src/test/java/com/stumeet/server/stub/StudyStub.java
@@ -2,8 +2,10 @@ package com.stumeet.server.stub;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.List;
 import org.springframework.mock.web.MockMultipartFile;
 
 import com.stumeet.server.study.application.port.in.command.StudyCreateCommand;
@@ -26,7 +28,7 @@ public class StudyStub {
         MockMultipartFile image = new MockMultipartFile("image", "test.jpg", "image/jpeg", "test".getBytes());
         return new StudyCreateCommand(
                 image,
-                1L,
+                "어학",
                 "영어 회화 클럽",
                 "매주 영어로 대화하며 언어 실력을 향상시키는 스터디 그룹입니다.",
                 "부산",
@@ -35,8 +37,45 @@ public class StudyStub {
                 LocalDate.parse("2024-12-15"),
                 LocalTime.parse("18:30:00"),
                 RepetitionType.valueOf("WEEKLY"),
-                Arrays.asList("월", "금"),
-                Arrays.asList("영어", "회화", "언어 교환")
+                List.of("월", "금"),
+                List.of("영어", "회화", "언어 교환")
+        );
+    }
+
+    public static StudyCreateCommand getInvalidFieldStudyCreateCommand() {
+        MockMultipartFile image = new MockMultipartFile("image", "test.jpg", "image/jpeg", "test".getBytes());
+        return new StudyCreateCommand(
+                image,
+                "존재하지 않는 분야 값",
+                "영어 회화 클럽",
+                "매주 영어로 대화하며 언어 실력을 향상시키는 스터디 그룹입니다.",
+                "부산",
+                "주 1회 대면 미팅 및 온라인 토론",
+                LocalDate.parse("2024-05-15"),
+                LocalDate.parse("2024-12-15"),
+                LocalTime.parse("18:30:00"),
+                RepetitionType.valueOf("WEEKLY"),
+                List.of("월", "금"),
+                List.of("영어", "회화", "언어 교환")
+        );
+    }
+
+    public static StudyCreateCommand getInvalidMeetingSchduleStudyCreateCommand() {
+        MockMultipartFile image = new MockMultipartFile("image", "test.jpg", "image/jpeg",
+                "test".getBytes());
+        return new StudyCreateCommand(
+                image,
+                "어학",
+                "영어 회화 클럽",
+                "매주 영어로 대화하며 언어 실력을 향상시키는 스터디 그룹입니다.",
+                "부산",
+                "주 1회 대면 미팅 및 온라인 토론",
+                LocalDate.parse("2024-05-15"),
+                LocalDate.parse("2024-12-15"),
+                LocalTime.parse("18:30:00"),
+                RepetitionType.valueOf("WEEKLY"),
+                List.of(),
+                List.of("영어", "회화", "언어 교환")
         );
     }
 }
--- src/test/resources/db/teardown.sql
+++ src/test/resources/db/teardown.sql
@@ -4,7 +4,6 @@ TRUNCATE TABLE member;
 TRUNCATE TABLE oauth_login;
 TRUNCATE TABLE study_member;
 TRUNCATE TABLE study_tag;
-TRUNCATE TABLE study_domain;
 TRUNCATE TABLE study;
 
 SET FOREIGN_KEY_CHECKS = 1;
