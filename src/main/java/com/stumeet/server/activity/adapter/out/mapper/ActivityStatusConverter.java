4f42f11ce75b424444d45776b5d68a644d00cac5
yongckim <zxcv9203@naver.com>
2024-04-15 21:54:01 +0900

    :sparkles: [STMT-254] 활동 상태 생성시 팩토리 클래스를 사용하는 방식으로 변경

--- src/main/java/com/stumeet/server/activity/adapter/out/mapper/ActivityStatusConverter.java
+++ src/main/java/com/stumeet/server/activity/adapter/out/mapper/ActivityStatusConverter.java
@@ -1,13 +1,8 @@
 package com.stumeet.server.activity.adapter.out.mapper;
 
-import com.stumeet.server.activity.domain.exception.NotExistsActivityStatusException;
 import com.stumeet.server.activity.domain.model.ActivityStatus;
-import com.stumeet.server.activity.domain.model.AssignmentStatus;
-import com.stumeet.server.activity.domain.model.DefaultStatus;
-import com.stumeet.server.activity.domain.model.MeetStatus;
+import com.stumeet.server.activity.domain.model.ActivityStatusFactory;
 import jakarta.persistence.AttributeConverter;
-
-import java.util.stream.Stream;
 
 public class ActivityStatusConverter implements AttributeConverter<ActivityStatus, String> {
     @Override
@@ -17,10 +12,6 @@ public class ActivityStatusConverter implements AttributeConverter<ActivityStatu
 
     @Override
     public ActivityStatus convertToEntityAttribute(String s) {
-        return Stream.of(MeetStatus.values(), DefaultStatus.values(), AssignmentStatus.values())
-                .flatMap(Stream::of)
-                .filter(activityStatus -> activityStatus.getStatus().equals(s))
-                .findAny()
-                .orElseThrow(() -> new NotExistsActivityStatusException(s));
+        return ActivityStatusFactory.createByStatus(s);
     }
 }
