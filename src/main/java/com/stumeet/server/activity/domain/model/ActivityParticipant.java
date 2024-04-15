4cfdf0e571336cbca1d8d60ead743863b29aa4fc
yongckim <zxcv9203@naver.com>
2024-04-15 20:35:52 +0900

    :sparkles: [STMT-254] 활동 참여 유저에 대한 도메인 정의

---
+++ src/main/java/com/stumeet/server/activity/domain/model/ActivityParticipant.java
@@ -0,0 +1,12 @@
+package com.stumeet.server.activity.domain.model;
+
+public class ActivityParticipant {
+
+    private Long id;
+
+    private Activity activity;
+
+    private ActivityMember member;
+
+    private ActivityStatus status;
+}
