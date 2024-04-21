3baa09443196549335bb9e5f3df3c23547fa99f5
05AM <anytime0224@gmail.com>
2024-04-21 23:59:01 +0900

    :white_check_mark: [STMT-168] 스터디 삭제 api 실패 테스트 케이스 작성

--- src/test/java/com/stumeet/server/study/adapter/in/web/StudyDeleteApiTest.java
+++ src/test/java/com/stumeet/server/study/adapter/in/web/StudyDeleteApiTest.java
@@ -44,5 +44,47 @@ class StudyDeleteApiTest extends ApiTest {
 						fieldWithPath("message").description("응답 메시지")
 					)));
 		}
+
+		@Test
+		@WithMockMember(id = 3)
+		@DisplayName("[실패] 삭제를 시도하는 멤버가 스터디 멤버가 아닌 경우 스터디 삭제에 실패한다.")
+		void fail_to_delete_when_member_not_joined_study() throws Exception {
+			mockMvc.perform(delete("/api/v1/studies/{id}", StudyStub.getStudyId())
+					.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+				.andExpect(status().isForbidden())
+				.andDo(document("delete-study/fail/member-not-admin",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestHeaders(headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description(
+						"서버로부터 전달받은 액세스 토큰")),
+					pathParameters(
+						parameterWithName("id").description("스터디 ID")
+					),
+					responseFields(
+						fieldWithPath("code").description("응답 상태"),
+						fieldWithPath("message").description("응답 메시지")
+					)));
+		}
+
+		@Test
+		@WithMockMember(id = 2)
+		@DisplayName("[실패] 삭제를 시도하는 멤버가 관리자가 아닌 경우 스터디 삭제에 실패한다.")
+		void fail_to_delete_when_study_member_is_not_admin() throws Exception {
+			mockMvc.perform(delete("/api/v1/studies/{id}", StudyStub.getStudyId())
+					.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+				.andExpect(status().isForbidden())
+				.andDo(document("delete-study/fail/member-not-joined_study",
+					preprocessRequest(prettyPrint()),
+					preprocessResponse(prettyPrint()),
+					requestHeaders(headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description(
+						"서버로부터 전달받은 액세스 토큰")),
+					pathParameters(
+						parameterWithName("id").description("스터디 ID")
+					),
+					responseFields(
+						fieldWithPath("code").description("응답 상태"),
+						fieldWithPath("message").description("응답 메시지")
+					)));
+		}
 	}
 }
