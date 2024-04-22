897ae091827e45150b32978a21252ff1b6b8487e
yongckim <zxcv9203@naver.com>
2024-04-22 20:06:21 +0900

    :recycle: [STMT-254] 활동 도메인을 생성하기 위한 생성자 및 빌더 추가

--- src/main/java/com/stumeet/server/activity/domain/model/Assignment.java
+++ src/main/java/com/stumeet/server/activity/domain/model/Assignment.java
@@ -1,5 +1,13 @@
 package com.stumeet.server.activity.domain.model;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
 public class Assignment extends Activity {
 
+    @Builder
+    protected Assignment(Long id, ActivityLinkedStudy study, ActivityMember author, ActivityCategory category, String title, String content, boolean isNotice, LocalDateTime startDate, LocalDateTime endDate, LocalDateTime createdAt) {
+        super(id, study, author, category, title, content, isNotice, startDate, endDate, createdAt);
+    }
 }
