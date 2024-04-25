86765527c95c2fbe3b8eb8e9e8846b3ec5d3471a
Lee ChanMi <anytime0224@gmail.com>
2024-04-25 20:36:56 +0900

    :construction: [STMT-168] 리뷰반영: jpa @modifying 쿼리 메서드에 transational 어노테이션 제거

--- src/main/java/com/stumeet/server/study/adapter/out/persistance/JpaStudyTagRepository.java
+++ src/main/java/com/stumeet/server/study/adapter/out/persistance/JpaStudyTagRepository.java
@@ -4,15 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.stumeet.server.study.adapter.out.persistance.entity.StudyTagJpaEntity;
 
 public interface JpaStudyTagRepository extends JpaRepository<StudyTagJpaEntity, Long> {
 
 	@Modifying
-	@Transactional(propagation = Propagation.MANDATORY)
 	@Query("DELETE FROM StudyTagJpaEntity s WHERE s.studyId = :studyId")
 	void deleteAllByStudyId(@Param("studyId") Long studyId);
 }
