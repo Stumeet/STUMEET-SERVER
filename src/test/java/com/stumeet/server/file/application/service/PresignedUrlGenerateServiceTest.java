0ad4c59f7e0cdcccd525b97bfe65c688bae4bcfe
yongckim <zxcv9203@naver.com>
2024-04-29 20:36:59 +0900

    :white_check_mark: [STMT-179] Presigned url 경로 생성 기능 테스트 코드 및 문서화 진행

---
+++ src/test/java/com/stumeet/server/file/application/service/PresignedUrlGenerateServiceTest.java
@@ -0,0 +1,57 @@
+package com.stumeet.server.file.application.service;
+
+import com.stumeet.server.file.application.port.dto.FileUrl;
+import com.stumeet.server.file.application.port.in.command.PresignedUrlCommand;
+import com.stumeet.server.file.application.port.in.response.PresignedUrlResponse;
+import com.stumeet.server.file.application.port.out.PresignedUrlGeneratePort;
+import com.stumeet.server.file.domain.exception.InvalidFileException;
+import com.stumeet.server.stub.FileStub;
+import com.stumeet.server.template.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.BDDMockito.given;
+
+class PresignedUrlGenerateServiceTest extends UnitTest {
+
+    @InjectMocks
+    private PresignedUrlGenerateService presignedUrlGenerateService;
+
+    @Mock
+    private PresignedUrlGeneratePort presignedUrlGeneratePort;
+
+    @Nested
+    @DisplayName("generatePresignedUrl 메소드는")
+    class GeneratePresignedUrl {
+
+        @Test
+        @DisplayName("[성공] PresignedUrl을 생성한다.")
+        void successTest() {
+            PresignedUrlCommand request = FileStub.getPresignedUrlCommand();
+            FileUrl want = FileStub.getPresignedUrl();
+
+            given(presignedUrlGeneratePort.generatePresignedUrl(request.path(), request.fileName()))
+                    .willReturn(want);
+
+            PresignedUrlResponse got = presignedUrlGenerateService.generatePresignedUrl(request);
+
+            assertThat(got.url()).isEqualTo(want.url());
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.stumeet.server.stub.FileStub#getInvalidFileTestArguments")
+        @DisplayName("[실패] 파일 이름이 유효하지 않은 경우 예외가 발생합니다.")
+        void invalidFileTest(String documentPath, PresignedUrlCommand invalidFileRequest, InvalidFileException e) {
+            assertThatCode(() -> presignedUrlGenerateService.generatePresignedUrl(invalidFileRequest))
+                    .isInstanceOf(e.getClass())
+                    .hasMessage(e.getMessage());
+        }
+    }
+}
--- src/test/java/com/stumeet/server/stub/FileStub.java
+++ src/test/java/com/stumeet/server/stub/FileStub.java
@@ -1,6 +1,13 @@
 package com.stumeet.server.stub;
 
+import com.stumeet.server.common.response.ErrorCode;
 import com.stumeet.server.file.application.port.dto.FileUrl;
+import com.stumeet.server.file.application.port.in.command.PresignedUrlCommand;
+import com.stumeet.server.file.domain.FileManagementPath;
+import com.stumeet.server.file.domain.exception.InvalidFileException;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
 
 public class FileStub {
     private FileStub() {
@@ -10,5 +17,38 @@ public class FileStub {
 
     public static FileUrl getFileUrl() {
         return new FileUrl("http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg");
+    }
+
+    public static FileUrl getPresignedUrl() {
+        return new FileUrl("http://127.0.0.1:52741/stumeet/activity/20240428213207105c0583-502b-4b59-8cc3-68a31eb9910f-test.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240428T123207Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=test%2F20240428%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=987d171e4e2bca89e7310f5d3e20dc9bf5e78b50a2510d0c0e7f2c05e0b8cc92");
+
+    }
+
+    public static PresignedUrlCommand getPresignedUrlCommand() {
+        return PresignedUrlCommand.builder()
+                .path(FileManagementPath.ACTIVITY)
+                .fileName("test.jpg")
+                .build();
+    }
+
+    public static Stream<Arguments> getInvalidFileTestArguments() {
+        return Stream.of(
+                Arguments.of(
+                        "presigned-url-generate/fail/invalid-file-name",
+                        PresignedUrlCommand.builder()
+                                .path(FileManagementPath.ACTIVITY)
+                                .fileName("test")
+                                .build(),
+                        new InvalidFileException(ErrorCode.INVALID_FILE_NAME_EXCEPTION)
+                ),
+                Arguments.of(
+                        "presigned-url-generate/fail/invalid-file-extension",
+                        PresignedUrlCommand.builder()
+                                .path(FileManagementPath.ACTIVITY)
+                                .fileName("test.pxp")
+                                .build(),
+                        new InvalidFileException(ErrorCode.INVALID_FILE_EXTENSION_EXCEPTION)
+                )
+        );
     }
 }
