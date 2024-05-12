9258c5b93e873541490aeebef707aa59dc28de50
yongckim <zxcv9203@naver.com>
2024-05-12 21:38:29 +0900

    :recycle: [STMT-179] 활동 생성 멤버 ID에 대한 변수명 수정

--- src/main/java/com/stumeet/server/activity/application/port/in/ActivityCreateUseCase.java
+++ src/main/java/com/stumeet/server/activity/application/port/in/ActivityCreateUseCase.java
@@ -3,5 +3,5 @@ package com.stumeet.server.activity.application.port.in;
 import com.stumeet.server.activity.application.port.in.command.ActivityCreateCommand;
 
 public interface ActivityCreateUseCase {
-    void create(Long studyId, ActivityCreateCommand command, Long id);
+    void create(Long studyId, ActivityCreateCommand command, Long memberId);
 }
