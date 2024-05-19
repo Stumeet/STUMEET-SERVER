998256d5758bb820861985d219b2cde00aa5e931
yongckim <zxcv9203@naver.com>
2024-05-19 19:33:35 +0900

    :wrench: [STMT-184] 스터디에 참여했으나 활동을 참여하지 않은 멤버 데이터 추가

--- src/test/resources/db/setup.sql
+++ src/test/resources/db/setup.sql
@@ -23,12 +23,16 @@ INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape) VALUES (2
 INSERT INTO member (id, name, image, region, profession_id, role, auth_type, tier, experience, is_deleted, deleted_at) VALUES (3, 'test3', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
         '서울', 1, 'MEMBER', 'OAUTH', 'SEED', 0.0, false, null);
 
+-- member id 4: study id 1을 참여했지만 활동은 참여하지 않은 멤버
+INSERT INTO member (id, name, image, region, profession_id, role, auth_type, tier, experience, is_deleted, deleted_at) VALUES (4, 'test4', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
+                                                                                                                               '서울', 1, 'MEMBER', 'OAUTH', 'SEED', 0.0, false, null);
+INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape) VALUES (4, 1, false, false);
 
 -- 스터디 활동 (기본 활동 - 공지)
-INSERT INTO activity(id, study_id, author_id, category, title, content, is_notice, start_date, end_date, location) VALUES (1, 1, 1, 'DEFAULT', '테스트 이름', '테스트 내용', true, '2024-04-01 18:00:00', '2024-04-01 20:00:00', '태릉입구역');
-INSERT INTO activity_image(id, activity_id, image) VALUES (1, 1, 'https://example.com/images/1');
-INSERT INTO activity_image(id, activity_id, image) VALUES (2, 1, 'https://example.com/images/2');
-INSERT INTO activity_image(id, activity_id, image) VALUES (3, 1, 'https://example.com/images/2');
+INSERT INTO activity(id, study_id, author_id, category, title, content, is_notice, start_date, end_date, location) VALUES (1, 1, 1, 'DEFAULT', 'title', 'content', true, '2024-04-01T00:00:00', '2050-05-01T00:00:00', null);
+INSERT INTO activity_image(id, activity_id, image) VALUES (1, 1, 'https://example.com/images/image1.png');
+INSERT INTO activity_image(id, activity_id, image) VALUES (2, 1, 'https://example.com/images/image2.png');
+INSERT INTO activity_image(id, activity_id, image) VALUES (3, 1, 'https://example.com/images/image3.png');
 INSERT INTO activity_participant(id, activity_id, member_id, status) VALUES (1, 1, 1, 'NONE');
 INSERT INTO activity_participant(id, activity_id, member_id, status) VALUES (2, 1, 2, 'NONE');
 
