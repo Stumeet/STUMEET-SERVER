abddcea4a05a4289908878162b3b010a24e7b940
yongckim <zxcv9203@naver.com>
2024-05-20 19:33:54 +0900

    :bug: [STMT-184] 줄바꿈으로 인한 테스트 쿼리 실행 실패 문제 해결

--- src/test/resources/db/setup.sql
+++ src/test/resources/db/setup.sql
@@ -15,14 +15,14 @@ VALUES (2, 'PROGRAMMING', 'future study', '서울', 'java 스터디 입니다.',
         'https://stumeet.s3.ap-northeast-2.amazonaws.com/study/1/image/2023062711172178420.png',
         '18:00:00', 'WEEKLY;월;수;목;', '2999-04-01', '2999-05-01');
 
-INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape)
-VALUES (1, 2, true, false);
+
 
 -- member id 1: study id 1의 멤버이자 관리자
 INSERT INTO member (id, name, image, region, profession_id, role, auth_type, tier, experience, is_deleted, deleted_at) VALUES (1, 'test1', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
         '서울', 1, 'MEMBER', 'OAUTH', 'SEED', 0.0, false, null);
 
 INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape) VALUES (1, 1, true, false);
+INSERT INTO study_member(member_id, study_id, is_admin, is_sent_grape) VALUES (1, 2, true, false);
 
 -- member id 2: study id 1의 멤버
 INSERT INTO member (id, name, image, region, profession_id, role, auth_type, tier, experience, is_deleted, deleted_at) VALUES (2, 'test2', 'http://localhost:4572/user/1/profile/2024030416531039839905-b7e8-4ad3-9552-7d9cbc01cb14-test.jpg',
