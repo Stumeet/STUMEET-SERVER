737389899aceacc6935bf23c44b85dac8e293497
Lee ChanMi <anytime0224@gmail.com>
2024-06-09 20:44:20 +0900

    :sparkles: [STMT-261] 활동 상세 리스트 조회 persistence adapter 구현

--- src/main/java/com/stumeet/server/activity/adapter/out/persistence/ActivityPersistenceAdapter.java
+++ src/main/java/com/stumeet/server/activity/adapter/out/persistence/ActivityPersistenceAdapter.java
@@ -1,33 +1,47 @@
 package com.stumeet.server.activity.adapter.out.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.stumeet.server.activity.adapter.out.mapper.ActivityPersistenceMapper;
 import com.stumeet.server.activity.adapter.out.model.ActivityJpaEntity;
 import com.stumeet.server.activity.application.port.out.ActivityCreatePort;
 import com.stumeet.server.activity.application.port.out.ActivityQueryPort;
 import com.stumeet.server.activity.domain.exception.NotExistsActivityException;
 import com.stumeet.server.activity.domain.model.Activity;
+import com.stumeet.server.activity.domain.model.ActivityCategory;
 import com.stumeet.server.common.annotation.PersistenceAdapter;
+
 import lombok.RequiredArgsConstructor;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
 public class ActivityPersistenceAdapter implements ActivityCreatePort, ActivityQueryPort {
 
-    private final JpaActivityRepository jpaActivityRepository;
-    private final ActivityPersistenceMapper activityPersistenceMapper;
+	private final JpaActivityRepository jpaActivityRepository;
+	private final ActivityPersistenceMapper activityPersistenceMapper;
 
-    @Override
-    public Activity create(Activity activity) {
-        ActivityJpaEntity entity = activityPersistenceMapper.toEntity(activity);
+	@Override
+	public Activity create(Activity activity) {
+		ActivityJpaEntity entity = activityPersistenceMapper.toEntity(activity);
 
-        return activityPersistenceMapper.toDomain(jpaActivityRepository.save(entity));
-    }
+		return activityPersistenceMapper.toDomain(jpaActivityRepository.save(entity));
+	}
 
-    @Override
-    public Activity getById(Long activityId) {
-        ActivityJpaEntity activityJpaEntity = jpaActivityRepository.findById(activityId)
-                .orElseThrow(() -> new NotExistsActivityException(activityId));
+	@Override
+	public Activity getById(Long activityId) {
+		ActivityJpaEntity entity = jpaActivityRepository.findById(activityId)
+				.orElseThrow(() -> new NotExistsActivityException(activityId));
 
-        return activityPersistenceMapper.toDomain(activityJpaEntity);
-    }
+		return activityPersistenceMapper.toDomain(entity);
+	}
+
+	@Override
+	public Page<Activity> getDetailPagesByCondition(
+			Pageable pageable, Boolean isNotice, Long studyId, ActivityCategory category) {
+		Page<ActivityJpaEntity> entities =
+				jpaActivityRepository.findDetailPagesByCondition(pageable, isNotice, studyId, category);
+
+		return activityPersistenceMapper.toDomainPages(entities);
+	}
 }
--- src/main/java/com/stumeet/server/activity/adapter/out/persistence/JpaActivityRepository.java
+++ src/main/java/com/stumeet/server/activity/adapter/out/persistence/JpaActivityRepository.java
@@ -3,5 +3,5 @@ package com.stumeet.server.activity.adapter.out.persistence;
 import com.stumeet.server.activity.adapter.out.model.ActivityJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaActivityRepository extends JpaRepository<ActivityJpaEntity, Long> {
+public interface JpaActivityRepository extends JpaRepository<ActivityJpaEntity, Long>, JpaActivityRepositoryCustom {
 }
