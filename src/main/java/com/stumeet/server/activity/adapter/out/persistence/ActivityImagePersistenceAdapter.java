224fd99475d32b2477b9ccc682a3ad827aaa2422
05AM <anytime0224@gmail.com>
2024-06-17 18:39:29 +0900

    :sparkles: [STMT-273] 활동 이미지 파일 데이터 삭제 구현

--- src/main/java/com/stumeet/server/activity/adapter/out/persistence/ActivityImagePersistenceAdapter.java
+++ src/main/java/com/stumeet/server/activity/adapter/out/persistence/ActivityImagePersistenceAdapter.java
@@ -2,6 +2,7 @@ package com.stumeet.server.activity.adapter.out.persistence;
 
 import com.stumeet.server.activity.adapter.out.mapper.ActivityImagePersistenceMapper;
 import com.stumeet.server.activity.adapter.out.model.ActivityImageJpaEntity;
+import com.stumeet.server.activity.application.port.out.ActivityImageCommandPort;
 import com.stumeet.server.activity.application.port.out.ActivityImageCreatePort;
 import com.stumeet.server.activity.application.port.out.ActivityImageQueryPort;
 import com.stumeet.server.activity.domain.model.ActivityImage;
@@ -12,7 +13,7 @@ import java.util.List;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
-public class ActivityImagePersistenceAdapter implements ActivityImageCreatePort, ActivityImageQueryPort {
+public class ActivityImagePersistenceAdapter implements ActivityImageCreatePort, ActivityImageQueryPort, ActivityImageCommandPort {
 
     private final JpaActivityImageRepository jpaActivityImageRepository;
     private final ActivityImagePersistenceMapper activityImagePersistenceMapper;
@@ -29,5 +30,10 @@ public class ActivityImagePersistenceAdapter implements ActivityImageCreatePort,
         return jpaActivityImageRepository.findAllByActivityId(activityId).stream()
                 .map(activityImagePersistenceMapper::toDomain)
                 .toList();
+    }
+
+    @Override
+    public void deleteAllByActivityId(Long activityId) {
+        jpaActivityImageRepository.deleteAllByActivityId(activityId);
     }
 }
