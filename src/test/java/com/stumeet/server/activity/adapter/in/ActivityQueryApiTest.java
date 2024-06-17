281026f37e5a17787ed9c3044cf1362a01c9b211
05AM <anytime0224@gmail.com>
2024-06-17 15:11:22 +0900

    :white_check_mark: [STMT-273] 활동 간략 목록 API 테스트 케이스 작성

--- src/test/java/com/stumeet/server/activity/adapter/in/ActivityQueryApiTest.java
+++ src/test/java/com/stumeet/server/activity/adapter/in/ActivityQueryApiTest.java
@@ -174,11 +174,11 @@ class ActivityQueryApiTest extends ApiTest {
 		@DisplayName("[성공] 스터디 활동 목록 상세 조회에 성공한다.")
 		void success() throws Exception {
 			mockMvc.perform(get(PATH)
-							.param("size", "2")
+							.param("size", "100")
 							.param("page", "0")
 							.param("isNotice", "true")
 							.param("studyId", StudyStub.getStudyId().toString())
-							.param("category", ActivityCategory.DEFAULT.name())
+							.param("category", ActivityCategory.MEET.name())
 							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
 					.andExpect(status().isOk())
 					.andDo(document("get-activity-details-by-condition/success",
@@ -270,6 +270,214 @@ class ActivityQueryApiTest extends ApiTest {
 									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
 									parameterWithName("studyId").description("특정 스터디 ID").optional(),
 									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지")
+							)
+					));
+		}
+	}
+
+	@Nested
+	@DisplayName("활동 목록 간략 조회 API")
+	class GetBriefsByCondition {
+		private static final String PATH = "/api/v1/studies/activities/brief";
+
+		@Test
+		@WithMockMember
+		@DisplayName("[성공] 스터디 활동 목록 간략 조회에 성공한다.")
+		void success() throws Exception {
+			mockMvc.perform(get(PATH)
+							.param("size", "100")
+							.param("page", "0")
+							.param("isNotice", "true")
+							.param("category", ActivityCategory.MEET.name())
+							.param("studyId", StudyStub.getStudyId().toString())
+							.param("fromDate", "2024-04-01T00:00:00")
+							.param("toDate", "2024-04-30T00:00:00")
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isOk())
+					.andDo(document("get-activity-briefs-by-condition/success",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지"),
+									fieldWithPath("data.items[]").description("활동 상세 목록"),
+									fieldWithPath("data.items[].id").description("활동 ID"),
+									fieldWithPath("data.items[].category").description("활동 유형"),
+									fieldWithPath("data.items[].title").description("활동 제목"),
+									fieldWithPath("data.items[].startDate").description("활동 시작일"),
+									fieldWithPath("data.items[].endDate").description("활동 종료일"),
+									fieldWithPath("data.items[].location").description("장소"),
+									fieldWithPath("data.items[].status").description("내 활동 상태"),
+									fieldWithPath("data.items[].createdAt").description("활동 생성일"),
+									fieldWithPath("data.pageInfo").description("페이지 메타 정보"),
+									fieldWithPath("data.pageInfo.totalPages").description("전체 페이지 수"),
+									fieldWithPath("data.pageInfo.totalElements").description("전체 요소 수"),
+									fieldWithPath("data.pageInfo.currentPage").description("현재 페이지"),
+									fieldWithPath("data.pageInfo.pageSize").description("페이지 크기")
+							)
+					));
+		}
+
+		@Test
+		@WithMockMember
+		@DisplayName("[실패] 불완전한 페이지네이션 요청변수로 요청하는 경우 활동 상세 조회에 실패한다.")
+		void fail_when_pagination_parameters_is_incomplete() throws Exception{
+			mockMvc.perform(get(PATH)
+							.param("size", "5")
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isBadRequest())
+					.andDo(document("get-activity-briefs-by-condition/fail/pagination-incomplete",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지")
+							)
+					));
+		}
+
+		@Test
+		@WithMockMember
+		@DisplayName("[실패] 0보다 작은 페이지네이션 요청변수로 요청하는 경우 활동 상세 조회에 실패한다.")
+		void fail_when_pagination_parameters_is_smaller_than_zero() throws Exception{
+			mockMvc.perform(get(PATH)
+							.param("size", "-1")
+							.param("page", "-1")
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isBadRequest())
+					.andDo(document("get-activity-briefs-by-condition/fail/pagination-invalid",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지")
+							)
+					));
+		}
+
+		@Test
+		@WithMockMember
+		@DisplayName("[실패] 존재하지 않는 스터디 id로 요청하는 경우 활동 상세 조회에 실패한다.")
+		void fail_when_study_id_not_found() throws Exception{
+			mockMvc.perform(get(PATH)
+							.param("studyId", StudyStub.getInvalidStudyId().toString())
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isNotFound())
+					.andDo(document("get-activity-briefs-by-condition/fail/study-not-found",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지")
+							)
+					));
+		}
+
+		@Test
+		@WithMockMember
+		@DisplayName("[실패] 유효하지 않은 활동 유형으로 요청하는 경우 활동 상세 조회에 실패한다.")
+		void fail_when_activity_category_not_found() throws Exception{
+			mockMvc.perform(get(PATH)
+							.param("category", ActivityStub.getInvalidActivityCategoryName())
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isBadRequest())
+					.andDo(document("get-activity-briefs-by-condition/fail/activity-category-invalid",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
+							),
+							responseFields(
+									fieldWithPath("code").description("응답 코드"),
+									fieldWithPath("message").description("응답 메시지")
+							)
+					));
+		}
+
+		@Test
+		@WithMockMember
+		@DisplayName("[실패] 시작기준일이 종료기준일보다 이후인 경우 활동 상세 조회에 실패한다.")
+		void fail_when_fromDate_is_after_toDate() throws Exception{
+			mockMvc.perform(get(PATH)
+							.param("fromDate", "2024-06-01T00:00:00")
+							.param("toDate", "2024-05-01T00:00:00")
+							.header(AuthenticationHeader.ACCESS_TOKEN.getName(), TokenStub.getMockAccessToken()))
+					.andExpect(status().isBadRequest())
+					.andDo(document("get-activity-briefs-by-condition/fail/period_invalid",
+							preprocessRequest(prettyPrint()),
+							preprocessResponse(prettyPrint()),
+							requestHeaders(
+									headerWithName(AuthenticationHeader.ACCESS_TOKEN.getName()).description("서버로부터 전달받은 액세스 토큰")
+							),
+							queryParameters(
+									parameterWithName("size").description("페이지당 결과 수").optional(),
+									parameterWithName("page").description("조회할 페이지 번호").optional(),
+									parameterWithName("isNotice").description("공지사항 여부 (true/false)").optional(),
+									parameterWithName("studyId").description("특정 스터디 ID").optional(),
+									parameterWithName("category").description("활동 유형 (DEFAULT/MEET/ASSIGNMENT)").optional(),
+									parameterWithName("fromDate").description("YYYY-MM-DDThh:mm:ss").optional(),
+									parameterWithName("toDate").description("YYYY-MM-DDThh:mm:ss").optional()
 							),
 							responseFields(
 									fieldWithPath("code").description("응답 코드"),
