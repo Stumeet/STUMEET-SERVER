ff1227598df661d1276b4cd325a6b174190cff4d
Lee ChanMi <anytime0224@gmail.com>
2024-07-26 22:18:13 +0900

    :sparkles: [STMT-263] activity participant update 기능 추가

--- src/main/java/com/stumeet/server/activity/application/port/out/ActivityParticipantCommandPort.java
+++ src/main/java/com/stumeet/server/activity/application/port/out/ActivityParticipantCommandPort.java
@@ -1,6 +1,12 @@
 package com.stumeet.server.activity.application.port.out;
 
+import java.util.List;
+
+import com.stumeet.server.activity.domain.model.ActivityParticipant;
+
 public interface ActivityParticipantCommandPort {
 
     void deleteByActivityId(Long activityId);
+
+    void update(Long activityId, List<ActivityParticipant> participants);
 }
