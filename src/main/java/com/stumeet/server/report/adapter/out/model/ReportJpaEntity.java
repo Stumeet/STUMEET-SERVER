2cf912859a6fb0a022c8b4bae9cc035520dba2ca
Lee ChanMi <anytime0224@gmail.com>
2024-08-07 16:54:37 +0900

    :sparkles: [STMT-267] 신고 jpa entity, repository 구현

---
+++ src/main/java/com/stumeet/server/report/adapter/out/model/ReportJpaEntity.java
@@ -0,0 +1,38 @@
+package com.stumeet.server.report.adapter.out.model;
+
+import com.stumeet.server.report.domain.ReportCategory;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class ReportJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated
+    private ReportCategory category;
+
+    private Long reportedId;
+
+    private Long reporterId;
+
+    private String title;
+
+    private String content;
+}
+
