efc7e7ed8a2b32ac0f7a1524dd45df246a769373
Lee ChanMi <anytime0224@gmail.com>
2024-08-12 20:39:08 +0900

    :sparkles: [STMT-267] 신고 조회 서비스 구현

--- src/main/java/com/stumeet/server/report/adapter/out/mapper/ReportPersistenceMapper.java
+++ src/main/java/com/stumeet/server/report/adapter/out/mapper/ReportPersistenceMapper.java
@@ -8,14 +8,26 @@ import com.stumeet.server.report.domain.Report;
 @Component
 public class ReportPersistenceMapper {
 
-    public ReportJpaEntity toEntity(Report report) {
+    public ReportJpaEntity toEntity(Report domain) {
         return ReportJpaEntity.builder()
-                .id(report.getId())
-                .category(report.getCategory())
-                .reason(report.getReason())
-                .reportedId(report.getReportedId())
-                .reporterId(report.getReporterId())
-                .content(report.getContent())
+                .id(domain.getId())
+                .category(domain.getCategory())
+                .reason(domain.getReason())
+                .reportedId(domain.getReportedId())
+                .reporterId(domain.getReporterId())
+                .content(domain.getContent())
+                .build();
+    }
+
+    public Report toDomain(ReportJpaEntity entity) {
+        return Report.builder()
+                .id(entity.getId())
+                .category(entity.getCategory())
+                .reason(entity.getReason())
+                .reportedId(entity.getReportedId())
+                .reporterId(entity.getReporterId())
+                .content(entity.getContent())
+                .createdAt(entity.getCreatedAt())
                 .build();
     }
 }
