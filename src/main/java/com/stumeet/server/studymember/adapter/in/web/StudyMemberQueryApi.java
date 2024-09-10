3e7bfc2900157d581554ef6be95010ca2323ac9f
Lee ChanMi <anytime0224@gmail.com>
2024-09-10 15:39:26 +0900

    :sparkles: [STMT-290] 스터디 멤버 상세 조회 API 구현

--- src/main/java/com/stumeet/server/studymember/adapter/in/web/StudyMemberQueryApi.java
+++ src/main/java/com/stumeet/server/studymember/adapter/in/web/StudyMemberQueryApi.java
@@ -4,9 +4,11 @@ import com.stumeet.server.common.annotation.WebAdapter;
 import com.stumeet.server.common.auth.model.LoginMember;
 import com.stumeet.server.common.model.ApiResponse;
 import com.stumeet.server.common.response.SuccessCode;
+import com.stumeet.server.studymember.application.port.in.response.StudyMemberDetailResponse;
 import com.stumeet.server.studymember.application.port.in.response.StudyMemberResponses;
 import com.stumeet.server.studymember.application.port.in.StudyMemberQueryUseCase;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -30,6 +32,19 @@ public class StudyMemberQueryApi {
         return new ResponseEntity<>(
                 ApiResponse.success(SuccessCode.GET_SUCCESS, responses),
                 HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/studies/{studyId}/members/{memberId}")
+    public ResponseEntity<ApiResponse<StudyMemberDetailResponse>> getStudyMemberDetail(
+            @AuthenticationPrincipal LoginMember member,
+            @PathVariable Long studyId,
+            @PathVariable Long memberId
+    ) {
+        StudyMemberDetailResponse response = studyMemberQueryUseCase.getStudyMemberDetail(studyId, memberId, member.getId());
+
+        return ResponseEntity.ok(
+                ApiResponse.success(SuccessCode.GET_SUCCESS, response)
         );
     }
 }
