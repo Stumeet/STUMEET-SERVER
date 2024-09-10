b0dc67b4eba83f72470c2f42de09e4746b723191
Lee ChanMi <anytime0224@gmail.com>
2024-09-10 15:38:49 +0900

    :sparkles: [STMT-290] 스터디 멤버 상세 조회 유스케이스 구현

---
+++ src/main/java/com/stumeet/server/studymember/application/port/in/response/StudyMemberDetailResponse.java
@@ -0,0 +1,12 @@
+package com.stumeet.server.studymember.application.port.in.response;
+
+public record StudyMemberDetailResponse(
+        Long id,
+        String name,
+        String image,
+        String region,
+        String profession,
+        boolean isSentGrape,
+        Integer achievement
+) {
+}
