bbd862630471aaa51b425c1cbfb4d5000a8ebf04
Lee ChanMi <anytime0224@gmail.com>
2024-10-07 14:37:29 +0900

    :adhesive_bandage: [STMT-258] 스터디 멤버 검증 메서드로 대체

--- src/main/java/com/stumeet/server/studymember/application/service/LegacyStudyHideService.java
+++ src/main/java/com/stumeet/server/studymember/application/service/LegacyStudyHideService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.stumeet.server.common.annotation.UseCase;
 import com.stumeet.server.study.application.port.in.StudyValidationUseCase;
 import com.stumeet.server.studymember.application.port.in.LegacyStudyHideUseCase;
+import com.stumeet.server.studymember.application.port.in.StudyMemberValidationUseCase;
 import com.stumeet.server.studymember.application.port.out.StudyMemberQueryPort;
 import com.stumeet.server.studymember.application.port.out.StudyMemberUpdatePort;
 import com.stumeet.server.studymember.application.port.out.StudyMemberValidationPort;
@@ -18,14 +19,14 @@ import lombok.RequiredArgsConstructor;
 public class LegacyStudyHideService implements LegacyStudyHideUseCase {
 
     private final StudyValidationUseCase studyValidationUseCase;
+    private final StudyMemberValidationUseCase studyMemberValidationUseCase;
 
     private final StudyMemberQueryPort studyMemberQueryPort;
-    private final StudyMemberValidationPort studyMemberValidationPort;
     private final StudyMemberUpdatePort studyMemberUpdatePort;
 
     @Override
     public void hideLegacyStudyForMember(Long studyId, Long memberId) {
-        studyMemberValidationPort.isNotStudyJoinMember(studyId, memberId);
+        studyMemberValidationUseCase.checkStudyJoinMember(studyId, memberId);
         studyValidationUseCase.checkLegacyStudy(studyId);
 
         StudyMember studyMember = studyMemberQueryPort.findStudyMember(studyId, memberId);
