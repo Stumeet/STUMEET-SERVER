062230cf31905aae3b719a44ed9a5fa386ca7734
Lee ChanMi <anytime0224@gmail.com>
2024-10-21 21:18:53 +0900

    :card_file_box: [STMT-300] DB 마이그레이션 파일 오타 수정

---
+++ src/main/resources/db/migration/V1.10__rename_notification_token_table.sql
@@ -0,0 +1,15 @@
+ALTER TABLE notification_token
+    RENAME device;
+
+ALTER TABLE device
+    CHANGE token notification_token VARCHAR(255) NOT NULL COMMENT '알림 토큰';
+
+ALTER TABLE device
+    DROP FOREIGN KEY `fk_registration_token_by_member_id`;
+
+ALTER TABLE device
+    ADD CONSTRAINT `fk_device_by_member_id`
+        FOREIGN KEY (`member_id`)
+            REFERENCES `member` (`id`)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE;
