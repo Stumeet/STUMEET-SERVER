30aaa460ac1251f0e0954c4f378f2faa7587f84b
Lee ChanMi <anytime0224@gmail.com>
2024-10-25 17:41:40 +0900

    :sparkles: [STMT-300] 스터디 가입 시 스터디 공지 구독 구현

--- src/main/java/com/stumeet/server/studymember/application/service/StudyMemberJoinService.java
+++ src/main/java/com/stumeet/server/studymember/application/service/StudyMemberJoinService.java
@@ -2,6 +2,7 @@ package com.stumeet.server.studymember.application.service;
 
 import com.stumeet.server.common.annotation.UseCase;
 import com.stumeet.server.member.application.port.in.MemberValidationUseCase;
+import com.stumeet.server.notification.application.port.in.SubscribeTopicUseCase;
 import com.stumeet.server.study.application.port.in.StudyValidationUseCase;
 import com.stumeet.server.studymember.application.port.in.StudyMemberJoinUseCase;
 import com.stumeet.server.studymember.application.port.in.StudyMemberValidationUseCase;
@@ -19,9 +20,12 @@ public class StudyMemberJoinService implements StudyMemberJoinUseCase {
 
     private final MemberValidationUseCase memberValidationUseCase;
     private final StudyValidationUseCase studyValidationUseCase;
-    private final StudyMemberUseCaseMapper studyMemberUseCaseMapper;
-    private final StudyMemberJoinPort studyMemberJoinPort;
     private final StudyMemberValidationUseCase studyMemberValidationUseCase;
+    private final SubscribeTopicUseCase subscribeTopicUseCase;
+
+    private final StudyMemberJoinPort studyMemberJoinPort;
+
+    private final StudyMemberUseCaseMapper studyMemberUseCaseMapper;
 
     @Override
     public void join(StudyMemberJoinCommand command) {
@@ -33,5 +37,7 @@ public class StudyMemberJoinService implements StudyMemberJoinUseCase {
 
         // todo: headcount 검사 및 인원 수 증가 로직 추가 필요
         studyMemberJoinPort.join(studyMember);
+
+        subscribeTopicUseCase.subscribeStudyNoticeTopic(command.memberId(), command.studyId());
     }
 }
