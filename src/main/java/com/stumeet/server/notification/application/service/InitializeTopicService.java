6bb03b95b4d32607504d1b701bdb86431899505f
Lee ChanMi <anytime0224@gmail.com>
2024-10-27 20:43:41 +0900

    :sparkles: [STMT-300] 알림 토픽 초기화 기능에 스터디 존재 검증, 토픽 중복 검증 로직 추가

--- src/main/java/com/stumeet/server/notification/application/service/InitializeTopicService.java
+++ src/main/java/com/stumeet/server/notification/application/service/InitializeTopicService.java
@@ -6,9 +6,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.stumeet.server.common.annotation.UseCase;
 import com.stumeet.server.notification.application.port.in.InitializeTopicUseCase;
+import com.stumeet.server.notification.application.port.in.TopicValidationUseCase;
 import com.stumeet.server.notification.application.port.out.SaveTopicPort;
 import com.stumeet.server.notification.domain.Topic;
 import com.stumeet.server.notification.domain.TopicType;
+import com.stumeet.server.study.application.port.in.StudyValidationUseCase;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,10 +21,16 @@ public class InitializeTopicService implements InitializeTopicUseCase {
     private static final String STUDY_NOTICE_DESCRIPTION_TEMPLATE = "ID: %d 스터디 공지 토픽";
     private static final String STUDY_NOTICE_TOPIC_TEMPLATE = "%d_STUDY_NOTICE_%s";
 
+    private final StudyValidationUseCase studyValidationUseCase;
+    private final TopicValidationUseCase topicValidationUseCase;
+
     private final SaveTopicPort saveTopicPort;
 
     @Override
     public void initializeStudyNoticeTopic(Long studyId) {
+        studyValidationUseCase.checkById(studyId);
+        topicValidationUseCase.validateUnique(TopicType.STUDY_NOTICE, studyId);
+
         String topicName = String.format(STUDY_NOTICE_TOPIC_TEMPLATE, studyId, UUID.randomUUID());
 
         Topic topic = Topic.builder()
