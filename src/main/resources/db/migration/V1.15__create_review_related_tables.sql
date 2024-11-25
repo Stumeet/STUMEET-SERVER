2defeff67b1485f67b88c45d617f11166fdf3b31
Lee ChanMi <anytime0224@gmail.com>
2024-11-25 20:06:01 +0900

    :card_file_box: [STMT-306] 리뷰 테이블 DDL 작성

---
+++ src/main/resources/db/migration/V1.15__create_review_related_tables.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS `review`
+(
+    `id`          BIGINT        NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '리뷰 ID',
+    `reviewer_id` BIGINT        NOT NULL COMMENT '리뷰 작성자 ID',
+    `reviewee_id` BIGINT        NOT NULL COMMENT '리뷰 대상자 ID',
+    `study_id`    BIGINT        NOT NULL COMMENT '스터디 ID',
+    `rate`        TINYINT       NOT NULL COMMENT '평점',
+    `content`     VARCHAR(1000) NULL COMMENT '리뷰 내용',
+    `created_at`  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    `updated_at`  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+    CONSTRAINT `fk_review_reviewer_id` FOREIGN KEY (`reviewer_id`)
+        REFERENCES `member` (`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_review_reviewee_id` FOREIGN KEY (`reviewee_id`)
+        REFERENCES `member` (`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_review_study_id` FOREIGN KEY (`study_id`)
+        REFERENCES `study` (`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `review_tag`
+(
+    `id`       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '리뷰 태그 ID',
+    `tag_name` VARCHAR(255) NOT NULL COMMENT '태그 이름'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `review_tag_usage`
+(
+    `id`            BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '리뷰 태그 사용 ID',
+    `review_id`     BIGINT NOT NULL COMMENT '리뷰 ID',
+    `review_tag_id` BIGINT NOT NULL COMMENT '리뷰 태그 ID',
+    CONSTRAINT `fk_review_tag_usage_review_id` FOREIGN KEY (`review_id`)
+        REFERENCES `review` (`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_review_tag_usage_review_tag_id` FOREIGN KEY (`review_tag_id`)
+        REFERENCES `review_tag` (`id`)
+        ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
