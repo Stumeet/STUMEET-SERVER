96450b7b3d37e0a5031b4296dfe8f1ef6cef59c0
Lee ChanMi <anytime0224@gmail.com>
2024-12-02 20:05:22 +0900

    :sparkles: [STMT-306] 리뷰 DB 저장 기능 구현

---
+++ src/main/java/com/stumeet/server/review/adapter/out/persistence/JpaReviewTagUsageRepository.java
@@ -0,0 +1,12 @@
+package com.stumeet.server.review.adapter.out.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stumeet.server.review.adapter.out.persistence.entity.ReviewTagUsageJpaEntity;
+
+public interface JpaReviewTagUsageRepository extends JpaRepository<ReviewTagUsageJpaEntity, Long> {
+
+    Optional<ReviewTagUsageJpaEntity> findByReviewTagTagName(String name);
+}
---
+++ src/main/java/com/stumeet/server/review/adapter/out/persistence/ReviewPersistenceAdapter.java
@@ -0,0 +1,51 @@
+package com.stumeet.server.review.adapter.out.persistence;
+
+import java.util.List;
+
+import com.stumeet.server.common.annotation.PersistenceAdapter;
+import com.stumeet.server.review.adapter.out.persistence.entity.ReviewJpaEntity;
+import com.stumeet.server.review.adapter.out.persistence.entity.ReviewTagJpaEntity;
+import com.stumeet.server.review.adapter.out.persistence.entity.ReviewTagUsageJpaEntity;
+import com.stumeet.server.review.adapter.out.persistence.mapper.ReviewPersistenceMapper;
+import com.stumeet.server.review.application.port.out.ReviewSavePort;
+import com.stumeet.server.review.domain.Review;
+import com.stumeet.server.review.domain.ReviewTag;
+
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class ReviewPersistenceAdapter implements ReviewSavePort {
+
+    private final JpaReviewRepository jpaReviewRepository;
+    private final JpaReviewTagRepository jpaReviewTagRepository;
+    private final JpaReviewTagUsageRepository jpaReviewTagUsageRepository;
+
+    private final ReviewPersistenceMapper reviewPersistenceMapper;
+
+    @Override
+    public void save(Review review) {
+        ReviewJpaEntity entity = reviewPersistenceMapper.toEntity(review);
+        entity = jpaReviewRepository.save(entity);
+
+        List<ReviewTagJpaEntity> reviewTagEntities = findReviewTags(review.getReviewTags());
+        saveReviewTags(entity, reviewTagEntities);
+    }
+
+    public List<ReviewTagJpaEntity> findReviewTags(List<ReviewTag> reviewTags) {
+        return reviewTags.stream()
+            .map(tag -> jpaReviewTagRepository.findByTagName(tag.getTagName()))
+            .toList();
+    }
+
+    public void saveReviewTags(ReviewJpaEntity review, List<ReviewTagJpaEntity> reviewTags) {
+        List<ReviewTagUsageJpaEntity> reviewTagUsageJpaEntities = reviewTags.stream()
+            .map(reviewTag -> ReviewTagUsageJpaEntity.builder()
+                .review(review)
+                .reviewTag(reviewTag)
+                .build())
+            .toList();
+
+        jpaReviewTagUsageRepository.saveAll(reviewTagUsageJpaEntities);
+    }
+}
