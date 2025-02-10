97edf6f95eb91814e570dc4c7e8f11c60c011f25
Lee ChanMi <anytime0224@gmail.com>
2025-02-10 18:59:36 +0900

    :sparkles: [STMT-290] 내 스터디 멤버 상세 정보 조회 유스케이스 구현

--- src/main/java/com/stumeet/server/studymember/application/port/in/StudyMemberQueryUseCase.java
+++ src/main/java/com/stumeet/server/studymember/application/port/in/StudyMemberQueryUseCase.java
@@ -2,7 +2,7 @@ package com.stumeet.server.studymember.application.port.in;
 
 import java.util.List;
 
-import com.stumeet.server.studymember.application.port.in.response.StudyMemberAdminResponse;
+import com.stumeet.server.studymember.application.port.in.response.MyStudyMemberDetailResponse;
 import com.stumeet.server.studymember.application.port.in.response.StudyMemberDetailResponse;
 import com.stumeet.server.studymember.application.port.in.response.StudyMemberResponses;
 import com.stumeet.server.studymember.application.port.in.response.StudyMemberReviewStatusResponse;
@@ -12,9 +12,11 @@ public interface StudyMemberQueryUseCase {
 
     StudyMemberDetailResponse getStudyMemberDetail(Long studyId, Long targetMemberId, Long requesterId);
 
-    StudyMemberAdminResponse isMemberAdmin(Long studyId, Long memberId);
-
-    boolean canSendGrape(Long studyId, Long memberId);
-
     List<StudyMemberReviewStatusResponse> getStudyMemberReviewStatusByMember(Long studyId, Long memberId);
+
+    MyStudyMemberDetailResponse getMyStudyMemberDetail(Long studyId, Long memberId);
+
+    long getStudyMemberCount(Long studyId);
+
+    boolean isMemberAdmin(Long studyId, Long memberId);
 }
